4f29f7716d3379a4cd13c55e38e3551128889357
Samuel David López Galeano <samuelitolopez1206@gmail.com>
2025-02-06 09:35:20 -0500

    Add files via upload

---
+++ Arbolitos.py
@@ -0,0 +1,66 @@
+#coding=utf-8
+
+#Programa para imprimir números del 1 al 10
+
+print('Programa para imprimir número del 1 al 10')
+
+#Partes de un ciclo de control
+# 1. Inicialización de la variable de control
+# 2. Condición de repetición 
+# 3. Sentencia de salida 
+
+# Primer arbolito: ladeado a la izquierda, de menor a mayor
+
+print('Primer arbolito: ladeado a la izquierda, de menor a mayor')
+nivel = 1
+while nivel <= 10:
+    print(nivel * '*')
+    nivel += 1
+
+print('Segundo arbolito: ladeado a la izquierda, de menos a mayor')
+
+nivel = 10
+while nivel >= 1:
+    print(nivel * '*')
+    nivel -= 1
+
+print('\nTercer arbolito: ladeado a la derecha, de menor a mayor')        
+
+total_niveles = 10
+cantidad_espacios = total_niveles - 1
+nivel = 1
+while nivel <= total_niveles:
+    print(' ' * cantidad_espacios + nivel * '*')
+    nivel += 1
+    cantidad_espacios -= 1
+
+print('\nCuarto arbolito: ladeado a la derecha, de mayor a menor')
+total_niveles
+cantidad_espacios = 0
+nivel = 10 #Variable de control
+while nivel > 0:
+    print(' ' * cantidad_espacios + nivel * '*')
+    nivel -= 1
+    cantidad_espacios += 1
+
+print('\nQuinto arbolito: centrado de menor a mayor')
+nivel = 1
+cantidad = 1
+cantidad_espacios = 10
+while nivel <= 20:
+    print(' ' * cantidad_espacios + nivel * '*')
+    nivel += 2
+    cantidad_espacios -= 1
+
+print('\nSexto arbolito: centrado de mayor a menor')
+nivel = 20
+cantidad = 1
+cantidad_espacios = 0
+while nivel > 0:
+    print(' ' * cantidad_espacios + nivel * '*')
+    nivel -= 2
+    cantidad_espacios += 1
+
+
+    
+    
